10ad2e56fdb42e84a89d173e868250a0ffb8ac69
tmknom <tmk.nom@gmail.com>
2018-11-17 23:29:06 +0900

    Implement minimal example

--- examples/minimal/main.tf
+++ examples/minimal/main.tf
@@ -1,1 +1,13 @@
+module "acm" {
+  source                 = "../../"
+  domain_name            = "${local.domain_name}"
+  route53_record_zone_id = "${data.aws_route53_zone.default.id}"
+}
 
+data "aws_route53_zone" "default" {
+  name = "${local.domain_name}."
+}
+
+locals {
+  domain_name = "example.com"
+}
--- examples/minimal/outputs.tf
+++ examples/minimal/outputs.tf
@@ -1,1 +1,19 @@
+output "acm_certificate_id" {
+  value = "${module.acm.acm_certificate_id}"
+}
 
+output "acm_certificate_arn" {
+  value = "${module.acm.acm_certificate_arn}"
+}
+
+output "acm_certificate_domain_validation_options" {
+  value = "${module.acm.acm_certificate_domain_validation_options}"
+}
+
+output "route53_record_fqdn" {
+  value = "${module.acm.route53_record_fqdn}"
+}
+
+output "route53_record_name" {
+  value = "${module.acm.route53_record_name}"
+}
