712e2357c3a9adf5b911577e518117ee5023a9e3
tmknom <tmk.nom@gmail.com>
2018-11-18 10:33:07 +0900

    Implement complete example

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -1,1 +1,24 @@
+module "acm" {
+  source                 = "../../"
+  domain_name            = "${local.domain_name}"
+  route53_record_zone_id = "${data.aws_route53_zone.default.id}"
 
+  route53_record_ttl = "120"
+
+  subject_alternative_names = [
+    "stg.${local.domain_name}",
+    "dev.${local.domain_name}",
+  ]
+
+  tags = {
+    Environment = "prod"
+  }
+}
+
+data "aws_route53_zone" "default" {
+  name = "${local.domain_name}."
+}
+
+locals {
+  domain_name = "example.com"
+}
