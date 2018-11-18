66f4a2724fc0b58b8febdc49600d5564c89bbac2
tmknom <tmk.nom@gmail.com>
2018-11-18 12:07:51 +0900

    Reflect variable name change

--- examples/minimal/main.tf
+++ examples/minimal/main.tf
@@ -1,7 +1,7 @@
 module "certificate" {
   source                 = "../../"
   domain_name            = "${local.domain_name}"
-  route53_record_zone_id = "${data.aws_route53_zone.default.id}"
+  zone_id = "${data.aws_route53_zone.default.id}"
 }
 
 data "aws_route53_zone" "default" {
