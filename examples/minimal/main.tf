198b4410526206b64c20b30dafa9e5c82cbbf648
Andrew Mackett <andrewm@yoyowallet.com>
2019-07-15 11:45:36 +0100

    Terraform 0.12 upgrade

--- examples/minimal/main.tf
+++ examples/minimal/main.tf
@@ -1,7 +1,7 @@
 module "certificate" {
   source      = "../../"
-  domain_name = "${local.domain_name}"
-  zone_id     = "${data.aws_route53_zone.default.id}"
+  domain_name = local.domain_name
+  zone_id     = data.aws_route53_zone.default.id
 }
 
 data "aws_route53_zone" "default" {
@@ -9,12 +9,13 @@ data "aws_route53_zone" "default" {
 }
 
 locals {
-  domain_name         = "${var.domain_name != "" ? var.domain_name : local.default_domain_name}"
+  domain_name         = var.domain_name != "" ? var.domain_name : local.default_domain_name
   default_domain_name = "example.com"
 }
 
 variable "domain_name" {
   default     = ""
-  type        = "string"
+  type        = string
   description = "If TF_VAR_domain_name set in the environment variables, then use that value."
 }
+
--- outputs.tf
+++ outputs.tf
@@ -1,24 +1,25 @@
 output "acm_certificate_id" {
-  value       = "${join("", aws_acm_certificate.default.*.id)}"
+  value       = join("", aws_acm_certificate.default.*.id)
   description = "The ARN of the certificate."
 }
 
 output "acm_certificate_arn" {
-  value       = "${join("", aws_acm_certificate.default.*.arn)}"
+  value       = join("", aws_acm_certificate.default.*.arn)
   description = "The ARN of the certificate."
 }
 
 output "acm_certificate_domain_validation_options" {
-  value       = "${flatten(aws_acm_certificate.default.*.domain_validation_options)}"
+  value       = flatten(aws_acm_certificate.default.*.domain_validation_options)
   description = "A list of attributes to feed into other resources to complete certificate validation."
 }
 
 output "route53_record_fqdns" {
-  value       = ["${aws_route53_record.default.*.fqdn}"]
+  value       = [aws_route53_record.default.*.fqdn]
   description = "FQDN built using the zone domain and name."
 }
 
 output "route53_record_names" {
-  value       = ["${aws_route53_record.default.*.name}"]
+  value       = [aws_route53_record.default.*.name]
   description = "The name of the record."
 }
+
